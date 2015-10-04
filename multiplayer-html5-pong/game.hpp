3d8c01586ebcbf5c50bde6bc01e21ec1f73c5695
Andy Thomason <andy@andythomason.com>
2015-10-04 13:58:19 +0100

    split into separate files

--- multiplayer-html5-pong/game.hpp
+++ multiplayer-html5-pong/game.hpp
@@ -2,67 +2,67 @@
 
 namespace game_server {
 
-enum class type {
-  bat = 0,
-  ball = 1,
-};
+  enum class type {
+    bat = 0,
+    ball = 1,
+  };
 
-class game_object {
-public:
-  game_object(float x, float y, float w, float h, type t) :
-    x(x), y(y), w(w), h(h), t(t)
-  {
-  }
-
-  // every frame, write the data to a string
-  // for rendering on the client.
-  void write(std::string &data) {
-    union u {
-      char c[20];
-      struct { float x, y, w, h; int t; } xy;
-    };
-    u ud;
-    ud.xy.x = x;
-    ud.xy.y = y;
-    ud.xy.w = w;
-    ud.xy.h = h;
-    ud.xy.t = (int)t;
-    data.append(ud.c, sizeof(u));
-  }
-private:
-  float x;
-  float y;
-  float w;
-  float h;
-  type t;
-};
-
-class game {
-public:
-  game() {
-    add(20, 20, 20, 100, type::bat);
-    add(1024-40, 20, 20, 100, type::bat);
-    add(512, 384, 20, 20, type::ball);
-  }
-
-  // simulate the game
-  void do_frame() {
-  }
-
-  // serialise the rendering state to a string.
-  void write(std::string &data) {
-    for (auto &obj : objects) {
-      obj.write(data);
+  class game_object {
+  public:
+    game_object(float x, float y, float w, float h, type t) :
+      x(x), y(y), w(w), h(h), t(t)
+    {
     }
-  }
-private:
-  game &add(float x, float y, float w, float h, type t) {
-    objects.emplace_back(x, y, w, h, t);
-    return *this;
-  }
 
-  std::vector<game_object> objects;
-};
+    // every frame, write the data to a string
+    // for rendering on the client.
+    void write(std::string &data) {
+      union u {
+        char c[20];
+        struct { float x, y, w, h; int t; } xy;
+      };
+      u ud;
+      ud.xy.x = x;
+      ud.xy.y = y;
+      ud.xy.w = w;
+      ud.xy.h = h;
+      ud.xy.t = (int)t;
+      data.append(ud.c, sizeof(u));
+    }
+  private:
+    float x;
+    float y;
+    float w;
+    float h;
+    type t;
+  };
+
+  class game {
+  public:
+    game() {
+      add(20, 20, 20, 100, type::bat);
+      add(1024-40, 20, 20, 100, type::bat);
+      add(512, 384, 20, 20, type::ball);
+    }
+
+    // simulate the game
+    void do_frame() {
+    }
+
+    // serialise the rendering state to a string.
+    void write(std::string &data) {
+      for (auto &obj : objects) {
+        obj.write(data);
+      }
+    }
+  private:
+    game &add(float x, float y, float w, float h, type t) {
+      objects.emplace_back(x, y, w, h, t);
+      return *this;
+    }
+
+    std::vector<game_object> objects;
+  };
 
 
 }
--- multiplayer-html5-pong/server.hpp
+++ multiplayer-html5-pong/server.hpp
@@ -1,59 +1,61 @@
 
 namespace game_server {
 
-class server {
-public:
-  server(boost::asio::io_service &io) :
-    io_(io),
-    acceptor_(io, tcp::endpoint(tcp::v4(), 8000)),
-    socket_(io)
-  {
-    // todo: add lobbying
-    games.push_back(new game());
-    do_accept();
-  }
+  using boost::asio::ip::tcp;
 
-  void do_accept() {
-    acceptor_.async_accept(socket_,
-      [this](boost::system::error_code ec)
-      {
-        // Check whether the server was stopped by a signal before this
-        // completion handler had a chance to run.
-        if (acceptor_.is_open() && !ec) {
-          // remove dead connections
-          size_t i = 0, j = 0;
-          for (; i != connections.size(); ++i) {
-            if (connections[i]->is_open()) {
-              connections[j++] = connections[i];
-            } else {
-              delete connections[i];
+  class server {
+  public:
+    server(boost::asio::io_service &io) :
+      io_(io),
+      acceptor_(io, tcp::endpoint(tcp::v4(), 8000)),
+      socket_(io)
+    {
+      // todo: add lobbying
+      games.push_back(new game());
+      do_accept();
+    }
+
+    void do_accept() {
+      acceptor_.async_accept(socket_,
+        [this](boost::system::error_code ec)
+        {
+          // Check whether the server was stopped by a signal before this
+          // completion handler had a chance to run.
+          if (acceptor_.is_open() && !ec) {
+            // remove dead connections
+            size_t i = 0, j = 0;
+            for (; i != connections.size(); ++i) {
+              if (connections[i]->is_open()) {
+                connections[j++] = connections[i];
+              } else {
+                delete connections[i];
+              }
             }
+            connections.resize(j);
+
+            // add new connection
+            connections.push_back(new connection(io_, std::move(socket_), games[0]));
           }
-          connections.resize(j);
-
-          // add new connection
-          connections.push_back(new connection(io_, std::move(socket_), games[0]));
+          do_accept();
         }
-        do_accept();
-      }
-    );
-  }
+      );
+    }
 
-private:
-  // we can have many people playing the game
-  std::vector<connection *> connections;
+  private:
+    // we can have many people playing the game
+    std::vector<connection *> connections;
 
-  // we can have many games.
-  std::vector<game *> games;
+    // we can have many games.
+    std::vector<game *> games;
 
-  /// The io used to perform asynchronous operations.
-  io_service &io_;
+    /// The io used to perform asynchronous operations.
+    io_service &io_;
 
-  /// Acceptor used to listen for incoming connections.
-  boost::asio::ip::tcp::acceptor acceptor_;
+    /// Acceptor used to listen for incoming connections.
+    boost::asio::ip::tcp::acceptor acceptor_;
 
-  /// The next socket to be accepted.
-  socket socket_;
-};
+    /// The next socket to be accepted.
+    socket socket_;
+  };
 
 }
